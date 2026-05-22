0da7ffbc8684212f43ee8da509ebe24c5673e628
satyamrathod6669 <116417188+satyamrathod6669@users.noreply.github.com>
2026-05-22 17:28:54 +0530

    Delete extension directory

--- extension/content.js
+++
@@ -1,8 +0,0 @@
-// This file runs on every page
-// It just sends the current URL to popup.js when needed
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "getUrl") {
-    sendResponse({ url: window.location.href });
-  }
-});
